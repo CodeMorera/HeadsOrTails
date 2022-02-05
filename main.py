1e3b37e9af005b3fd1e466924e175ba78932d1cb
Chris <80273421+CodeMorera@users.noreply.github.com>
2022-02-05 03:46:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+test_seed=int(input("Create a seed number for the coin toss!: "))
+random.seed(test_seed)
+coin_toss=random.randint(0,1)
+if coin_toss ==1:
+  print("Heads")
+else:
+  print("Tails")
+
+
+
+
+
+
+
